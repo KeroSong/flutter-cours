99980862144e81a4eb1e727db2446f27c7b04ba7
Henrique Dong <donghenrique@gmail.com>
2023-06-05 18:01:58 +0200

    maj menu

--- lib/ui/menu.dart
+++ lib/ui/menu.dart
@@ -23,13 +23,34 @@ class _MenuScreenState extends State<MenuScreen> {
         color: const Color.fromARGB(255, 141, 205, 255),
         child: Column(
           children: [
-            const Center(child: Text("Choix de la liste")),
+            const Center(
+                child: Text("Choix de la liste",
+                    style: TextStyle(
+                      fontSize: 20, // Taille du texte
+                      color: Color.fromARGB(
+                          255, 255, 255, 255), // Couleur du texte
+                      fontWeight: FontWeight.bold, // Gras
+                    ))),
             for (int i = 0; i < listeprincipal.principalliste.length; i++)
-              ElevatedButton(
-                onPressed: () {
-                  game(context, listeprincipal.principalliste[i].menuliste);
-                },
-                child: Text(listeprincipal.principalliste[i].name),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    game(context, listeprincipal.principalliste[i].menuliste);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(
+                        255, 82, 175, 252), // Couleur de fond du bouton
+                  ),
+                  child: Text(
+                    listeprincipal.principalliste[i].name,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
               ),
           ],
         ),
